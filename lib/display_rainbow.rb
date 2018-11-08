e8e548d1eb54658510b28c423f78f34c4727ec9d
Edwin Lin <edwinlin@gmail.com>
2018-11-08 19:58:40 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,9 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(array)
+  temp = []
   var i = 0
   while i < array.length
-    puts "#{array[i][0].upcase}: array[i]"
+    temp << "#{array[i][0].upcase}: array[i]"
   end
 end
